698a3c976daac1c9251cf6a1c6a1dcb16c130865
Jacek Kubiak <jacek.ku@poczta.fm>
2026-01-13 19:38:38 +0100

    bookStore part2 to check

--- src/main/java/mate/academy/service/BookService.java
+++ src/main/java/mate/academy/service/BookService.java
@@ -1,10 +1,18 @@
 package mate.academy.service;
 
 import java.util.List;
+import mate.academy.dto.BookDto;
+import mate.academy.dto.CreateBookRequestDto;
 import mate.academy.model.Book;
 
 public interface BookService {
     Book save(Book book);
 
     List<Book> findAll();
+
+    List<BookDto> getAll();
+
+    BookDto getById(Long id);
+
+    BookDto create(CreateBookRequestDto bookDto);
 }
--- src/main/java/mate/academy/service/BookServiceImpl.java
+++ src/main/java/mate/academy/service/BookServiceImpl.java
@@ -1,7 +1,12 @@
 package mate.academy.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import mate.academy.dto.BookDto;
+import mate.academy.dto.CreateBookRequestDto;
+import mate.academy.mapper.BookMapper;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
 import org.springframework.stereotype.Service;
@@ -11,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
     public Book save(Book book) {
@@ -20,6 +26,28 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<Book> findAll() {
         return bookRepository.findAll();
+    }
+
+    @Override
+    public List<BookDto> getAll() {
+        return bookRepository.findAll()
+                .stream()
+                .map(bookMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public BookDto getById(Long id) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with id " + id));
+        return bookMapper.toDto(book);
+    }
+
+    @Override
+    public BookDto create(CreateBookRequestDto bookDto) {
+        Book book = bookMapper.toModel(bookDto);
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toDto(savedBook);
     }
 }
 
